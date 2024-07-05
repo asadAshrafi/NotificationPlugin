6658cdd4b20342ff695da3f1a9c27d14441432fd
Asad <asad.ashrafee@gmail.com>
2024-07-06 00:08:36 +0500

    console app removed

--- ConsoleApp1/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿using NotificationPlugin;
-
-//var emailNotification = new EmailNotification("", 503, "", "", "", "");
-//var smsNotification = new SmsNotification(emailNotification, "123456789");
-//var slackNotification = new SlackNotification(smsNotification, "slack_webhook_url");
-//slackNotification.Send("This is a dummy message.");
-
-var slackNotification = new SlackNotification("slack_webhook_url");
-slackNotification.Send("This is a dummy message.");
